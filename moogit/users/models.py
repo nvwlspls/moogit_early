77de8afac1ca7aaccb1fdaabaf485cc5f92b57f8
wayne jessen <mremeat@gmail.com>
2014-10-16 23:46:54 -0700

    added some model stuff

--- moogit/users/models.py
+++ moogit/users/models.py
@@ -4,8 +4,11 @@ from django.contrib.auth.models import AbstractUser
 
 # Import the basic Django ORM models library
 from django.db import models
+# from django.contrib.localflavor.us.forms import USPhoneNumberField
 
 from django.utils.translation import ugettext_lazy as _
+
+
 
 
 # Subclass AbstractUser
@@ -13,3 +16,24 @@ class User(AbstractUser):
 
     def __unicode__(self):
         return self.username
+
+class Venue(models.Model):
+	AREA_CHOICES = (
+		("NC", "North County"),
+		("EC", "East County"),
+		("CC", "Central"),
+		("SB", "South Boy")	
+	)
+
+
+	venueID = models.AutoField(primary_key = True)
+	venueName = models.CharField(max_length = 75)
+	description = models.TextField()
+	area = models.CharField(choices =  AREA_CHOICES,
+							max_length = 20)
+	Neighborhood = models.CharField(max_length = 75)
+	dateAdded = models.DateTimeField(auto_now_add = True)
+	lastSaved = models.DateTimeField(auto_now = True)
+	streetAddress = models.CharField(max_length = 150)
+	city = models.CharField(max_length = 150)
+	# state = USPhoneNumberField()
